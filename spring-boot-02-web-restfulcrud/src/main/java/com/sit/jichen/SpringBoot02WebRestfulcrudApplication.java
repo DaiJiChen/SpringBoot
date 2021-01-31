a336f3df87ce3896ba442c9d89b0404d293c621b
DaiJichen <1245487099@qq.com>
2021-01-30 22:08:25 -0500

    webjar

---
+++ spring-boot-02-web-restfulcrud/src/main/java/com/sit/jichen/SpringBoot02WebRestfulcrudApplication.java
@@ -0,0 +1,13 @@
+package com.sit.jichen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02WebRestfulcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot02WebRestfulcrudApplication.class, args);
+	}
+
+}
---
+++ spring-boot-02-web-restfulcrud/src/test/java/com/sit/jichen/SpringBoot02WebRestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sit.jichen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot02WebRestfulcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
