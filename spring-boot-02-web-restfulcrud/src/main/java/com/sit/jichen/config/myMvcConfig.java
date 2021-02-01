559652ba7dda574aa8eeed160a9b95d976a59edc
DaiJichen <1245487099@qq.com>
2021-02-01 01:24:30 -0500

    resource bundle message source

--- spring-boot-02-web-restfulcrud/src/main/java/com/sit/jichen/config/myMvcConfig.java
+++ spring-boot-02-web-restfulcrud/src/main/java/com/sit/jichen/config/myMvcConfig.java
@@ -1,5 +1,6 @@
 package com.sit.jichen.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +11,8 @@ public class myMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/jichen").setViewName("Hello");
     }
+
+    @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         WebMvcConfigurer adapter = new WebMvcConfigurer() {
             @Override
